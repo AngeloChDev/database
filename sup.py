76ad960b92f072a454941a630bc1b875e68e71ce
AngeloChDev <angeloch.dev@gmail.com>
2023-02-09 16:36:51 +0100

    init

---
+++ sup.py
@@ -0,0 +1,42 @@
+from bs4  import BeautifulSoup
+import requests
+import sqlite3
+
+url= "https://www.wg-gesucht.de/1-zimmer-wohnungen-in-Hamburg.55.1.1.0.html"
+result = requests.get(url)                                                     ####  CONNECT THE URL
+doc = BeautifulSoup(result.text, "html.parser")                                ### EXSTRACT HTML CODE
+cards = doc.find_all('div', attrs={ 'class':'middle'})                         ### SEARCH DATA 
+div_address = doc.find_all('div', attrs={ 'class':'col-xs-11'})                
+metri = doc.find_all('div', attrs={ 'class':'col-xs-3 text-right'})
+db= sqlite3.connect('sq2.db')                                                  ## CONNECT TO THE DATABASE 
+cursor = db.cursor()
+cursor.execute('''
+             CREATE TABLE IF NOT EXISTS DATA(
+                 QUARTIERE TEXT ,
+                 METRI TEXT ,
+                 PREZZO TEXT
+                )
+            ''')                                                             ## CREATE TABLE
+                                                                            ##CREATE TABLE
+addr = []                                                                   ### CREATE LIST WHERE INSERT DATA
+costo =[]
+metr2 =[]
+for address in div_address:                                                 ### TAKE THE VALUE
+    span = address.find('span')
+    s = span.text[150:]
+    cursor.execute("INSERT INTO DATA(QUARTIERE) VALUES(?)",(s,))
+    addr.append(s)
+for card in cards:                                                               ### TAKE THE VALUE
+    eur = card.find('b').get_text()
+    
+    valore = str(eur).split('ab')[0] 
+    costo.append(valore)
+    cursor.execute("INSERT INTO DATA(PREZZO) VALUES(?)",(valore,))
+    print(valore)
+for metro in metri:                                                                  ### TAKE THE VALUE
+    m2 = metro.find('b').get_text()
+    metr2.append(m2)
+    cursor.execute("INSERT INTO DATA(METRI) VALUES(?)",(m2,))
+#cursor.execute("INSERT INTO DATA(QUARTIERE,METRI,PREZZO) VALUES(?,?,?)",(s,))          ## INSERT VALUES IN TABLE
+db.commit()    
+  
---
+++ sup2.py
@@ -0,0 +1,63 @@
+from bs4  import BeautifulSoup
+import requests
+import sqlite3
+import json
+
+url= "https://www.wg-gesucht.de/1-zimmer-wohnungen-in-Hamburg.55.1.1.0.html"
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
+cards = doc.find_all('div', attrs={ 'class':'middle'})
+div_address = doc.find_all('div', attrs={ 'class':'col-xs-11'})
+metri = doc.find_all('div', attrs={ 'class':'col-xs-3 text-right'})
+db= sqlite3.connect('sq.db')
+cursor = db.cursor()
+cursor.execute('''
+             CREATE TABLE IF NOT EXISTS DATA(
+                 QUARTIERE TEXT ,
+                 METRI TEXT ,
+                 PREZZO TEXT
+                )
+            ''')
+addr = []
+costo =[]
+metr2 =[]
+for address in div_address:
+    span = address.find('span')
+    s = span.text[150:]
+    addr.append(s)
+for card in cards:
+    eur = card.find('b').get_text()
+    valore = str(eur).split('ab')[0] 
+    costo.append(valore)
+    print(valore)
+for metro in metri:
+    m2 = metro.find('b').get_text()
+    metr2.append(m2)  
+def tab(): 
+    for i in range(22): 
+        for n in addr :    
+            nn =n
+        for x in metr2:
+            xx =x
+        for y in costo:
+            yy =y
+    return cursor.execute("INSERT INTO DATA(QUARTIERE,METRI,PREZZO) VALUES(?,?,?)",(nn, xx, yy ) )
+tab()
+db.commit()    
+print(len(addr))
+print(len(costo))
+print(len(metr2))   
+
+#cursor.execute("INSERT INTO DATA(QUARTIERE,METRI,PREZZO) VALUES(?,?,?)")
+#db.commit()
+
+
+
+
+
+ 
+
+
+
+
+
